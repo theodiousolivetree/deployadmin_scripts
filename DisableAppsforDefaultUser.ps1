f685675252d6184548918fdc54480e3fbc8d3d86
theodiousolivetree <40150021+theodiousolivetree@users.noreply.github.com>
2018-07-13 16:03:57 +0200

    Create DisableAppsforDefaultUser.ps1

---
+++ DisableAppsforDefaultUser.ps1
@@ -0,0 +1,5 @@
+CMD /C reg load HKU\Default_User C:\Users\Default\NTUSER.DAT
+Set-ItemProperty -Path Registry::HKU\Default_User\SOFTWARE\Microsoft\Windows\CurrentVersion\ContentDeliveryManager -Name SystemPaneSuggestionsEnabled -Value 0
+Set-ItemProperty -Path Registry::HKU\Default_User\SOFTWARE\Microsoft\Windows\CurrentVersion\ContentDeliveryManager -Name PreInstalledAppsEnabled -Value 0
+Set-ItemProperty -Path Registry::HKU\Default_User\SOFTWARE\Microsoft\Windows\CurrentVersion\ContentDeliveryManager -Name OemPreInstalledAppsEnabled -Value 0
+CMD /C reg unload HKU\Default_User
