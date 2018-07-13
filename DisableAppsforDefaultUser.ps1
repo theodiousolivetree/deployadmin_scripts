a252cf31f2ea03b7641769dd9e0034804ebdf309
theodiousolivetree <40150021+theodiousolivetree@users.noreply.github.com>
2018-07-13 16:06:13 +0200

    Update DisableAppsforDefaultUser.ps1
    Claims Added

--- DisableAppsforDefaultUser.ps1
+++ DisableAppsforDefaultUser.ps1
@@ -1,3 +1,8 @@
+# Note: Some inspiration was taken from https://reddit/r/sysadmin
+# Set Set-ExecuionPolicy unrestricted
+# Disable Apps for Default User
+# By The Odious Olivetree
+# July 13 2018 
 CMD /C reg load HKU\Default_User C:\Users\Default\NTUSER.DAT
 Set-ItemProperty -Path Registry::HKU\Default_User\SOFTWARE\Microsoft\Windows\CurrentVersion\ContentDeliveryManager -Name SystemPaneSuggestionsEnabled -Value 0
 Set-ItemProperty -Path Registry::HKU\Default_User\SOFTWARE\Microsoft\Windows\CurrentVersion\ContentDeliveryManager -Name PreInstalledAppsEnabled -Value 0
