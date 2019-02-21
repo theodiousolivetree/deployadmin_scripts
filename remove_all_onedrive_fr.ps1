c88f8d2386119109df5ba265a08144a0a471e0c1
Olivier <40150021+theodiousolivetree@users.noreply.github.com>
2019-02-21 16:30:47 +0100

    new files
    new files

--- remove_all_onedrive_fr.ps1
+++ remove_all_onedrive_fr.ps1
@@ -1,8 +1,13 @@
-# Note: Some inspiration was taken from here: https://gist.github.com/ThaddeusAid/55a137fb48fc01594eca4e89a025b456
+# Note: Some inspiration was taken from here: https://gitlab.com/aapjeisbaas/Debloat-Windows-10/blob/694cd4f7f66f3cd8a7e7e463c536475aa119ce31/scripts/remove-onedrive.ps1
+# The computer needs being in a workgroup (no domain AD)
+# Set Set-ExecuionPolicy unrestricted
 # Remove ALL OneDrive
 # By The Odious Olivetree
 # July 4 2018 
-# Disable One Drive
+# Terminate any OneDrive processes
+Get-Process "onedrive" | Stop-process 
+    CMD /C "C:\Windows\System32\taskkill.exe /f /im OneDrive.exe"
+    CMD /C "C:\Windows\System32\taskkill.exe /f /im explorer.exe"
 # Uninstall
     CMD /C "c:\Windows\SysWOW64\OneDriveSetup.exe /uninstall"
 # take ownership of the re-installer
@@ -10,21 +15,35 @@
     CMD /C "icacls C:\windows\syswow64\OneDriveSetup.exe /grant Administrateurs:F"
 # Remove
     Remove-Item C:\Windows\SysWOW64\OneDriveSetup.exe -Force
-# Create (a fake .exe, 0 bytes in size)
+# Create a fake .exe, 0 bytes in size)
     New-Item C:\Windows\SysWOW64\OneDriveSetup.exe -Force
 # Deny system write
-    CMD /C "icacls C:\Windows\SysWOW64\OneDriveSetup.exe /deny Everyone:W"
+    CMD /C "icacls C:\Windows\SysWOW64\OneDriveSetup.exe /deny "Tout le Monde":W"
 
-
-write-Host "***Disabling OneDrive...***"
+# Disable OneDrive
+# write-Host "***Disabling OneDrive...***"
 reg add "HKLM\Software\Policies\Microsoft\Windows\OneDrive" /f /t REG_DWORD /v DisableFileSyncNGSC /d 1
 reg add "HKLM\Software\Policies\Microsoft\Windows\OneDrive" /f /t REG_DWORD /v DisableFileSync /d 1
 reg add "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\StartupApproved\Run" /f /t REG_BINARY /v OneDrive /d 0300000021B9DEB396D7D001
+
+# No OneDrive for newuser
+reg load "hku\Default" "C:\Users\Default\NTUSER.DAT"
+reg delete "HKEY_USERS\Default\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" /v "OneDriveSetup" /f
+reg unload "hku\Default"
+
 # Remove 'Onedrive folder' from left side of file explorer
 reg Add "HKLM\Software\Classes\CLSID\{018D5C66-4533-4307-9B53-224DE2ED1FE6}" /T REG_DWORD /V "System.IsPinnedToNameSpaceTree" /D 0 /F
+
+
 
 # Clean up random onedrive stuff
 Remove-Item -Recurse -Force -ErrorAction SilentlyContinue "$env:localappdata\Microsoft\OneDrive"
 Remove-Item -Recurse -Force -ErrorAction SilentlyContinue "$env:programdata\Microsoft OneDrive"
 Remove-Item -Recurse -Force -ErrorAction SilentlyContinue "C:\OneDriveTemp"
 Remove-Item -Force -ErrorAction SilentlyContinue "$env:userprofile\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\OneDrive.lnk"
+
+# Do not let Windows fool you. 
+foreach ($item in (ls "$env:WinDir\WinSxS\*onedrive*")) {
+    Takeown-Folder $item.FullName
+    rm -Recurse -Force $item.FullName
+}
---
+++ removeappsfor all-v1.ps1
@@ -0,0 +1,173 @@
+# =======================================================
+# NAME: removeappsforall-v1.ps1
+# AUTHOR: Pavilla Olivier, Deployadmin.com
+# Aka The Odious Olivetree
+# DATE: 19/02/2019
+#
+# KEYWORDS: remove, Apps , default
+# VERSION 1.0
+# DD/MM/YYYY added help to the functions
+# COMMENTS: Removing default apps for all users especially default user
+# Set Set-ExecutionPolicy unrestricted
+#Requires -Version 2.0
+# =======================================================
+# first run as test:
+# Set-variable -name skypebloat$ -Value (get-appxpackage *skype* | select packagefullname)
+# remove-appxprovisionedpackage -online -packagename skypebloat$
+# Remove-AppxPackage -allusers -Package "testzune"
+# end of first run as test
+Set-ExecutionPolicy Unrestricted -Force Get-ExecutionPolicy
+Set-ExecutionPolicy -Scope LocalMachine -ExecutionPolicy Unrestricted -Force
+# Set-ExecutionPolicy -Scope MachinePolicy -ExecutionPolicy Unrestricted -Force
+
+# To uninstall 3D Builder:
+get-appxpackage -allusers *3dbuilder* | remove-appxpackage 
+
+# To uninstall Alarms & Clock:
+get-appxpackage -allusers *alarms* | remove-appxpackage 
+
+# To uninstall App Connector:
+get-appxpackage -allusers *appconnector* | remove-appxpackage 
+
+# To uninstall App Installer:
+# get-appxpackage *appinstaller* | remove-appxpackage
+
+# To uninstall Calendar and Mail apps together:
+get-appxpackage -allusers *communicationsapps* | remove-appxpackage 
+
+# To uninstall Calculator:
+get-appxpackage -allusers *calculator* | remove-appxpackage 
+
+# To uninstall Camera:
+get-appxpackage -allusers *camera* | remove-appxpackage 
+
+# To uninstall Feedback Hub:
+get-appxpackage -allusers *feedback* | remove-appxpackage 
+
+# To uninstall Get Office:
+get-appxpackage -allusers *officehub* | remove-appxpackage
+
+# To uninstall Get Started or Tips:
+get-appxpackage -allusers *getstarted* | remove-appxpackage
+
+# To uninstall Get Skype:
+get-appxpackage -allusers *skypeapp* | remove-appxpackage
+
+# To uninstall Groove Music:
+get-appxpackage -allusers *zunemusic* | remove-appxpackage
+
+# To uninstall Groove Music and Movies & TV apps together:
+get-appxpackage -allusers *zune* | remove-appxpackage
+
+# To uninstall Maps:
+get-appxpackage -allusers *maps* | remove-appxpackage
+
+# To uninstall Messaging and Skype Video apps together:
+get-appxpackage -allusers *messaging* | remove-appxpackage
+
+# To uninstall Microsoft Solitaire Collection:
+get-appxpackage -allusers *solitaire* | remove-appxpackage
+
+# To uninstall Microsoft Wallet:
+get-appxpackage -allusers *wallet* | remove-appxpackage
+
+# To uninstall Microsoft Wi-Fi:
+get-appxpackage -allusers *connectivitystore* | remove-appxpackage
+
+# To uninstall Zune Music, Movies & TV:
+get-appxpackage -allusers *zunevideo* | remove-appxpackage 
+get-appxpackage -allusers *zunemusic* | remove-appxpackage 
+
+# To uninstall OneNote:
+get-appxpackage -allusers *onenote* | remove-appxpackage 
+
+# To uninstall Paid Wi-Fi & Cellular:
+get-appxpackage -allusers *oneconnect* | remove-appxpackage 
+
+# To uninstall Paint 3D:
+get-appxpackage -allusers *mspaint* | remove-appxpackage 
+
+# To uninstall People:
+get-appxpackage -allusers *people* | remove-appxpackage 
+
+# To uninstall Phone, Phone Companion :
+get-appxpackage -allusers *commsphone* | remove-appxpackage 
+get-appxpackage -allusers *windowsphone* | remove-appxpackage
+# To uninstall Phone and Phone Companion apps together:
+# get-appxpackage -allusers *phone* | remove-appxpackage
+
+# To uninstall Photos:
+get-appxpackage -allusers *photos* | remove-appxpackage
+
+# To uninstall Sticky Notes:
+get-appxpackage -allusers *sticky* | remove-appxpackage
+
+# To uninstall Sway:
+get-appxpackage -allusers *sway* | remove-appxpackage
+
+# To uninstall View 3D:
+get-appxpackage -allusers *Microsoft3dviewer* | remove-appxpackage
+get-appxpackage -allusers *3d* | remove-appxpackage
+
+# To uninstall Voice Recorder:
+get-appxpackage -allusers *soundrecorder* | remove-appxpackage
+
+# To uninstall Bing Weather, Sports, Money & News :
+get-appxpackage -allusers *bingweather* | remove-appxpackage
+get-appxpackage -allusers *bingsports* | remove-appxpackage
+get-appxpackage -allusers *bingnews* | remove-appxpackage
+get-appxpackage -allusers *bingfinance* | remove-appxpackage
+# To uninstall all bing apps together at least :
+get-appxpackage -allusers *bing* | remove-appxpackage 
+
+# To uninstall Windows Holographic:
+get-appxpackage -allusers *holographic* | remove-appxpackage
+
+# To uninstall Windows Store: (Be very careful!)
+get-appxpackage -allusers *windowsstore* | remove-appxpackage
+
+# To uninstall apps added after W10 1511 
+Get-AppxPackage *phototastic* | Remove-AppxPackage
+Get-AppxPackage *picsart* | Remove-AppxPackage 
+Get-AppxPackage *plex* | Remove-AppxPackage 
+Get-AppxPackage *fitbitcoach* | Remove-AppxPackage 
+Get-AppxPackage *dolbyaccess* | Remove-AppxPackage
+
+# To uninstall Xbox:
+Get-AppxPackage -allusers *Xbox.TCUI* | Remove-AppxPackage -AllUsers
+Get-AppxPackage -allusers *XboxSpeechToTextOverlay* | Remove-AppxPackage
+Get-AppxPackage -allusers *XboxGameCallableUI* | Remove-AppxPackage -AllUsers
+Get-AppxPackage -allusers *XboxGamingOverlay* | Remove-AppxPackage -AllUsers
+Get-AppxPackage -allusers *XboxApp* | Remove-AppxPackage -AllUsers
+Get-AppxPackage -allusers *XboxGameOverlay* | Remove-AppxPackage -AllUsers
+Get-AppxPackage -allusers *XboxIdentityProvider* | Remove-AppxPackage -AllUsers
+# Get-appxpackage -allusers *xbox* | remove-appxpackage -AllUsers
+
+# Disable Apps for Default User
+CMD /C reg load HKU\Default_User C:\Users\Default\NTUSER.DAT
+Set-ItemProperty -Path Registry::HKU\Default_User\SOFTWARE\Microsoft\Windows\CurrentVersion\ContentDeliveryManager -Name SystemPaneSuggestionsEnabled -Value 0
+Set-ItemProperty -Path Registry::HKU\Default_User\SOFTWARE\Microsoft\Windows\CurrentVersion\ContentDeliveryManager -Name PreInstalledAppsEnabled -Value 0
+Set-ItemProperty -Path Registry::HKU\Default_User\SOFTWARE\Microsoft\Windows\CurrentVersion\ContentDeliveryManager -Name OemPreInstalledAppsEnabled -Value 0
+# opt
+Set-ItemProperty -Path Registry::HKU\Default_User\Software\Microsoft\Windows\CurrentVersion\ContentDeliveryManager -Name ContentDeliveryAllowed -Value 0
+Set-ItemProperty -Path Registry::HKU\Default_User\Software\Microsoft\Windows\CurrentVersion\ContentDeliveryManager -Name PreInstalledAppsEverEnabled -Value 0
+Set-ItemProperty -Path Registry::HKU\Default_User\Software\Microsoft\Windows\CurrentVersion\ContentDeliveryManager -Name SilentInstalledAppsEnabled -Value 0
+Set-ItemProperty -Path Registry::HKU\Default_User\Software\Microsoft\Windows\CurrentVersion\ContentDeliveryManager -Name SoftLandingEnabled -Value 0
+Set-ItemProperty -Path Registry::HKU\Default_User\Software\Microsoft\Windows\CurrentVersion\ContentDeliveryManager -Name SubscribedContentEnabled -Value 0
+# Set-ItemProperty -Path Registry::HKU\Default_User\Software\Microsoft\Windows\CurrentVersion\ContentDeliveryManager\Subscriptions
+# Set-ItemProperty -Path Registry::HKU\Default_User\Software\Microsoft\Windows\CurrentVersion\ContentDeliveryManager\SuggestedApps
+# opt
+CMD /C reg unload HKU\Default_User
+
+# Disable Cortana
+Function DisableCortana
+{  
+    $path = "HKLM:\SOFTWARE\Policies\Microsoft\Windows\Windows Search"    
+    IF(!(Test-Path -Path $path)) { 
+        New-Item -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows" -Name "Windows Search"
+    } 
+    Set-ItemProperty -Path $path -Name "AllowCortana" -Value 0 
+    # Do Restart Explorer to change it immediately    
+    Stop-Process -name explorer
+}
+DisableCortana
